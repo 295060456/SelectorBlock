6a4627ae3fd83b5e3992843adb531539bc7f6c45
Jobs <lg295060456@gmail.com>
2021-03-15 15:26:16 +0700

    no message

--- SelectorBlock/UI/ViewController/ViewController.m
+++ SelectorBlock/UI/ViewController/ViewController.m
@@ -38,8 +38,35 @@ UITableViewDelegate
 // NSInvocation的使用，方法多参数传递
 -(void)touchesBegan:(NSSet<UITouch *> *)touches
           withEvent:(UIEvent *)event{
-    NSString *methodNameStr = @"test:withArg2:andArg3:block:";
-    SEL selector = NSSelectorFromString(methodNameStr);
+    
+    NSString *arg1 = @"a";
+    NSString *arg2 = @"b";
+    NSString *arg3 = @"c";
+    MKDataBlock arg4 = ^(id data){
+        NSLog(@"嗯，不错");
+    };
+    
+    NSArray *paramarrays = @[arg1,
+                             arg2,
+                             arg3,
+                             arg4];
+    
+    [NSObject methodName:@"test:withArg2:andArg3:block:"
+                  target:self
+             paramarrays:paramarrays];
+    
+//    [self methodName:@"test:withArg2:andArg3:block:"
+//              target:self
+//         paramarrays:paramarrays];
+}
+/// NSInvocation的使用，方法多参数传递
+/// @param methodName 方法名
+/// @param target 靶点，方法在哪里
+/// @param paramarrays 参数数组
+-(void)methodName:(NSString *)methodName
+           target:(id _Nonnull)target
+      paramarrays:(NSArray *)paramarrays{
+    SEL selector = NSSelectorFromString(methodName);
     /*
      NSMethodSignature有两个常用的只读属性
      a. numberOfArguments:方法参数的个数
@@ -51,30 +78,20 @@ UITableViewDelegate
     
     //只能使用该方法来创建，不能使用alloc init
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-    invocation.target = self;
+    invocation.target = target;
     invocation.selector = selector;
-    
-    NSString *arg1 = @"a";
-    NSString *arg2 = @"b";
-    NSString *arg3 = @"c";
-    
-    MKDataBlock block = ^(id data){
-        NSLog(@"嗯，不错");
-    };
-
-    MKDataBlock arg4 = block;
     /*
      注意:
      1、下标从2开始，因为0、1已经被target与selector占用
      2、设置参数，必须传递参数的地址，不能直接传值
      **/
-    [invocation setArgument:&arg1 atIndex:2];
-    [invocation setArgument:&arg2 atIndex:3];
-    [invocation setArgument:&arg3 atIndex:4];
-    [invocation setArgument:&arg4 atIndex:5];
+    for (int i = 2; i < paramarrays.count + 2; i++) {
+        NSLog(@"i = %d",i);
+        id d = paramarrays[i - 2];
+        [invocation setArgument:&d atIndex:i];
+    }
     // 执行方法
     [invocation invoke];
-    
     //可以在invoke方法前添加，也可以在invoke方法后添加
     //通过方法签名的methodReturnLength判断是否有返回值
     if (signature.methodReturnLength > 0) {
@@ -93,6 +110,53 @@ UITableViewDelegate
         block(@"嗯！！");
     }
     return @"gaga";
+}
+/// 最基本的方法
+-(void)baseMethod{//
+    NSString *methodNameStr = @"test:withArg2:andArg3:block:";
+    SEL selector = NSSelectorFromString(methodNameStr);
+    /*
+     NSMethodSignature有两个常用的只读属性
+     a. numberOfArguments:方法参数的个数
+     b. methodReturnLength:方法返回值类型的长度，大于0表示有返回值
+     **/
+    NSMethodSignature *signature = [self methodSignatureForSelector:selector];
+    //或使用下面这种方式
+    //NSMethodSignature *signature = [[self class] instanceMethodSignatureForSelector:selector];
+
+    //只能使用该方法来创建，不能使用alloc init
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.target = self;
+    invocation.selector = selector;
+
+    NSString *arg1 = @"a";
+    NSString *arg2 = @"b";
+    NSString *arg3 = @"c";
+
+    MKDataBlock block = ^(id data){
+        NSLog(@"嗯，不错");
+    };
+
+    MKDataBlock arg4 = block;
+    /*
+     注意:
+     1、下标从2开始，因为0、1已经被target与selector占用
+     2、设置参数，必须传递参数的地址，不能直接传值
+     **/
+    [invocation setArgument:&arg1 atIndex:2];
+    [invocation setArgument:&arg2 atIndex:3];
+    [invocation setArgument:&arg3 atIndex:4];
+    [invocation setArgument:&arg4 atIndex:5];
+    // 执行方法
+    [invocation invoke];
+
+    //可以在invoke方法前添加，也可以在invoke方法后添加
+    //通过方法签名的methodReturnLength判断是否有返回值
+    if (signature.methodReturnLength > 0) {
+        NSString *result = nil;
+        [invocation getReturnValue:&result];
+        NSLog(@"result = %@",result);
+    }
 }
 #pragma mark —————————— UITableViewDelegate,UITableViewDataSource ——————————
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
